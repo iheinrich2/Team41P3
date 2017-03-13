0ce387a64c6c1389abe945d0bfe738d5d94a3568
Isaac Heinrich <iheinrich@wisc.edu>
2017-03-13 12:26:25 -0500

    added p3 files

---
+++ ThesaurusRecord.java
@@ -0,0 +1,77 @@
+import java.util.Comparator;
+
+/**
+ * The ThesaurusRecord class is the child class of Record to be used when
+ * merging thesaurus data.
+ */
+
+public class ThesaurusRecord extends Record {
+	// TODO declare data structures required
+
+	/**
+	 * Constructs a new ThesaurusRecord by passing the parameter to the parent
+	 * constructor and then calling the clear method()
+	 */
+	public ThesaurusRecord(int numFiles) {
+		super(numFiles);
+		clear();
+	}
+
+	/**
+	 * This Comparator should simply behave like the default (lexicographic)
+	 * compareTo() method for Strings, applied to the portions of the FileLines'
+	 * Strings up to the ":" The getComparator() method of the ThesaurusRecord
+	 * class will simply return an instance of this class.
+	 */
+	private class ThesaurusLineComparator implements Comparator<FileLine> {
+		public int compare(FileLine l1, FileLine l2) {
+
+			String s1 = l1.getString();
+			String s2 = l2.getString();
+
+			s1 = s1.split(":")[0];
+			s2 = s2.split(":")[0];
+
+			return s1.compareTo(s2);
+			
+		}
+
+		public boolean equals(Object o) {
+			return this.equals(o);
+		}
+	}
+
+	/**
+	 * This method should simply create and return a new instance of the
+	 * ThesaurusLineComparator class.
+	 */
+	public Comparator<FileLine> getComparator() {
+		return new ThesaurusLineComparator();
+	}
+
+	/**
+	 * This method should (1) set the word to null and (2) empty the list of
+	 * synonyms.
+	 */
+	public void clear() {
+		// TODO initialize/reset data members
+	}
+
+	/**
+	 * This method should parse the list of synonyms contained in the given
+	 * FileLine and insert any which are not already found in this
+	 * ThesaurusRecord's list of synonyms.
+	 */
+	public void join(FileLine w) {
+		// TODO implement join() functionality
+	}
+
+	/**
+	 * See the assignment description and example runs for the exact output
+	 * format.
+	 */
+	public String toString() {
+		// TODO
+		return null;
+	}
+}
