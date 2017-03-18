68e12ed120d821a7e3c20aaf9a3caf9c7e4a4ad8
jbenson6 <jbenson6@wisc.edu>
2017-03-18 11:31:38 -0500

    Update WeatherRecord.java

--- WeatherRecord.java
+++ WeatherRecord.java
@@ -1,3 +1,15 @@
+/////////////////////////////////////////////////////////////////////////////
+// Semester:         CS367 Spring 2017
+// PROJECT:          team41_p3
+// FILE:             WeatherRecord
+//
+// TEAM:    Team 41, IDGAF
+// Authors: 
+// Author1: (Jarrett Benson, jbenson6@wisc.edu, jbenson6, Lec 002)
+// Author2: (Cameron Carlson, ccarlson24@wisc.edu, ccarlson, Lec 002) 
+// Author3: (Isaac Heinrich, iheinrich@wisc.edu, iheinrich, Lec 002)  
+///////////////////////////////////////////////////////////////////////////////
+import java.util.ArrayList;
 import java.util.Comparator;
 
 /**
@@ -7,7 +19,11 @@ import java.util.Comparator;
  */
 public class WeatherRecord extends Record{
     // TODO declare data structures required
-
+	FileLine line;
+	private int station;
+	int date;
+	private ArrayList<Double> readings;
+	
 	/**
 	 * Constructs a new WeatherRecord by passing the parameter to the parent constructor
 	 * and then calling the clear method()
@@ -23,9 +39,24 @@ public class WeatherRecord extends Record{
 	 */
     private class WeatherLineComparator implements Comparator<FileLine> {
 		public int compare(FileLine l1, FileLine l2) {
-			// TODO implement compare() functionality
+			//split the first line in order to access station and date information
+			String[] lineFile1 = l1.getString().split(",");
+			String[] lineFile2 = l2.getString().split(",");
 			
-			return 0;
+			//assign station information in order to compare
+			Integer station1 = Integer.parseInt(lineFile1[0]);
+			Integer station2 = Integer.parseInt(lineFile2[0]);
+			
+			//check to see if the stations are the same. If so, return the compared dates.
+			if (station1.equals(station2)){
+				Integer date1 = Integer.parseInt(lineFile1[1]);
+				Integer date2 = Integer.parseInt(lineFile2[1]);
+				
+				return date1.compareTo(date2);
+			}
+			else{
+				return station1.compareTo(station2);
+			}
 		}
 		
 		public boolean equals(Object o) {
@@ -47,6 +78,13 @@ public class WeatherRecord extends Record{
 	 */
     public void clear() {
 		// TODO initialize/reset data members
+    	try{
+    		for (int i=0; i<readings.size(); i++)
+    			readings.set(i, Double.MIN_VALUE);
+    	}catch(NullPointerException e){
+    		//nullPointerException is caught
+    	}
+    		
     }
 
 	/**
@@ -57,7 +95,36 @@ public class WeatherRecord extends Record{
 	 * WeatherRecord should be set to the station and date values which were similarly parsed.
 	 */
     public void join(FileLine li) {
-		// TODO implement join() functionality
+    	
+    	//create an array of the strings in FileLine line
+    	String[] line = li.getString().split(",");
+    	
+    	if (station == 0 && date == 0 && readings.size() == 0) {
+    		this.line = li;
+    		this.station = Integer.parseInt(line[0]);
+    		this.date = Integer.parseInt(line[1]);
+    		for (int i = 2; i < line.length; i++){
+    			readings.add(Double.parseDouble(line[i]));
+    			
+    			//if I took the data from the top of the file containing FileLine li, iterate li's iterator
+    			li.getFileIterator().next();
+    		}
+    	}
+    	else{
+    		if (Integer.parseInt(li.getString().split(",")[0]) == this.station && Integer.parseInt(li.getString().split(",")[1]) == this.date){
+    			for (int i = 2; i < line.length; i++){
+    				readings.add(Double.parseDouble(line[i]));
+    				
+    				//if I took the data from the top of the file containing FileLine li, iterate li's iterator
+    				li.getFileIterator().next();
+    			}
+    		}
+    		//if the station and date does not contain a reading for this file (aka reading type (i.e. DEWP, TEMP, etc.)), add a
+    		//null element to be turned into a '-' later.
+    		else readings.add(null);
+    	}
+    	
+    	
     }
 	
 	/**
@@ -65,7 +132,9 @@ public class WeatherRecord extends Record{
 	 */
     public String toString() {
 		// TODO
-		
+		for (int i = 0; i < readings.size(); i++){
+			System.out.print(station + "," + date + "," + readings.get(i));
+		}
 		return null;
     }
 }
